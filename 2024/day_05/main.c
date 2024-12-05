a08d6057a37cc0925da68bb4151051f9d9d8e379
Thomas Metais <thomas.metais@etu.univ-nantes.fr>
2024-12-06 00:21:10 +0100

    add day 5 part 2

--- 2024/day_05/main.c
+++ 2024/day_05/main.c
@@ -65,9 +65,11 @@ int main()
   // printf("\n");
 
   int sum_part1 = 0;
+  int sum_part2 = 0;
   for (int i = 0; i < row; i++) // Parcours les lignes
   {
     bool valid = true;
+    bool valid_part2 = false;
     int max_col = 0;
     for (int j = 0; each_update[i][j] != 0 && j < 23; j++)
     {
@@ -84,21 +86,17 @@ int main()
           {
             if (each_update[i][l] == rules_right[k])
             {
+              int tochange1 = each_update[i][j];
+              int tochange2 = each_update[i][l];
               // printf("Erreur détectée à la ligne %d, colonne %d car la règle est %d|%d\n", i, j, rules_left[k], rules_right[k]);
               printf("%d|%d ", rules_left[k], rules_right[k]);
+              each_update[i][j] = tochange2;
+              each_update[i][l] = tochange1;
+              j = 1;
               valid = false;
+              valid_part2 = true;
             }
           }
-          if (each_update[i][j] == rules_right[k])
-            for (int m = 23; m > j; m--) // Parcours les règles colonne gauche
-            {
-              if (each_update[i][m] == rules_left[k])
-              {
-                // printf("Erreur détectée à la ligne %d, colonne %d car la règle est %d|%d\n", i, j, rules_left[k], rules_right[k]);
-                printf("%d|%d ", rules_left[k], rules_right[k]);
-                valid = false;
-              }
-            }
         }
       }
       max_col = j;
@@ -109,9 +107,21 @@ int main()
       sum_part1 += each_update[i][(max_col / 2)];
       printf("Ligne %d valide avec la page du milieu = %d", i + 1, each_update[i][(max_col / 2)]);
     }
+    if (valid_part2)
+    {
+      sum_part2 += each_update[i][(max_col / 2)];
+      printf("\n");
+      for (int j = 0; each_update[i][j] != 0 && j < 23; j++)
+      {
+        printf("%d ", each_update[i][j]);
+      }
+      printf("\nLigne %d corrigée avec la page du milieu = %d", i + 1, each_update[i][(max_col / 2)]);
+    }
     printf("\n\n");
   }
   printf("Somme des pages du milieu valide = %d\n", sum_part1);
+  printf("Somme des pages du milieu corrigée = %d\n", sum_part2);
+  printf("Somme des pages du milieu = %d\n", sum_part1 + sum_part2);
 
   return EXIT_SUCCESS;
 }
